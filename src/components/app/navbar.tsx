77d4a192d1638916e53c69208db707e49ff58462
ranepaarth <2019paarth.rane@ves.ac.in>
2024-06-07 00:59:56 +0530

    feat: main navbar with logout button

---
+++ src/components/app/navbar.tsx
@@ -0,0 +1,39 @@
+import { useLogoutUserMutation } from "@/features/api/authApiSlice";
+import { logout } from "@/features/userSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../ui/button";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [logoutUser] = useLogoutUserMutation();
+
+  const handleClick = async () => {
+    console.log("clicked");
+    try {
+      dispatch(logout());
+      await logoutUser("").then(() => {
+        navigate("/login");
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <nav className="w-full bg-white p-4 flex justify-between items-center border-b  shadow-sm">
+      <a href="/" className="text-xl md:text-2xl font-bold">
+        TeachTimely 📚
+      </a>
+      <Button
+        variant={"default"}
+        className="bg-destructive text-destructive-foreground hover:bg-red-600 md:text-lg"
+        onClick={handleClick}
+      >
+        Log out
+      </Button>
+    </nav>
+  );
+};
+
+export default Navbar;
