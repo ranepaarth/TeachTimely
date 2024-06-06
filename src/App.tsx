981b876bae2457bf9937f206b357f1503abf77c9
ranepaarth <2019paarth.rane@ves.ac.in>
2024-06-06 15:29:10 +0530

    feat: implement auth routing
    Implemented Auth routes using `react-router-dom`

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import AuthLayout from "./layouts/auth-layout";
+import HomePage from "./pages/home-page";
+import LoginPage from "./pages/login-page";
+import SignUpPage from "./pages/signup-page";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route element={<AuthLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" index element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Route>
+    </>
+  )
+);
+
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
